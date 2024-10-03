5bb8b46936501b067631fcd75287f54eebb2b733
Adam Brady <adam@boxxen.org>
2024-10-03 22:12:34 +1000

    identity updates

--- identity/pup.nix
+++ identity/pup.nix
@@ -2,8 +2,8 @@
 
 let
   identity_upstream = pkgs.callPackage (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/49efa12c50aa522df81f3fe4501efea9a0fdc01c/pkgs/identity/default.nix";
-    sha256 = "sha256-J3MsKK4Ir1g/YjIcJoa9OTepenw+5yjmqWi/x9l06ag=";
+    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/1112abf7b364c620192ccc3d92c0c99ec3890071/pkgs/identity/default.nix";
+    sha256 = "sha256-NDMxR+jpSZLosvwAFmFGektItf2iRclI1pshBaHJHXc=";
   }) {};
 
   ui = pkgs.fetchgit {
@@ -12,26 +12,10 @@ let
     sha256 = "sha256-xvm0O7+PLBHjPZVDLx5jkQO2Wja4dcgj26DA5hd7cRc=";
   };
 
-  combinedFiles = pkgs.stdenv.mkDerivation {
-    name = "identity-combined-files";
-    src = identity_upstream;
-
-    buildInputs = [ pkgs.bash ];
-
-    # installPhase = ''
-    #   mkdir -p $out/bin/web
-    #   mkdir -p $out/bin/storage
-    #   cp -r ${identity_upstream}/* $out/
-    #   cp -r ${ui}/src/* $out/bin/web/
-    # '';
-  };
-
   identity = pkgs.writeScriptBin "run.sh" ''
     #!${pkgs.bash}/bin/bash
-    mkdir -p /storage/storage/web
-    cp -r ${ui}/src/* /storage/storage/web
-    cd /storage
-    KEY=3aa47e2a7527ee7282c965c202c10ac1eaba2695b6ed508f979378df833c6648 ${combinedFiles}/bin/identity
+    export KEY=`cat /storage/delegated.key`
+    ${identity_upstream}/bin/identity --bind ''${DBX_PUP_IP} --web ${ui}/src --dir /storage
   '';
 in
 {
