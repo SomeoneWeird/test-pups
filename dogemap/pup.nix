6ca31104175bc8a7de2b97f1ecde6c126bf75a6e
Raffe <andrew@dogecoin.com>
2024-10-04 11:55:59 +1000

    dogemap depends on dogenet-web-api-nodes

--- dogemap/pup.nix
+++ dogemap/pup.nix
@@ -8,13 +8,13 @@ let
 
   ui = pkgs.fetchgit {
     url = "https://github.com/dogeorg/dogemap-ui.git";
-    rev = "3f83fe702ad955d9f654e3edf05201b2b9950388";
-    sha256 = "sha256-xID2IDyT83xgQ0f9izbpCktUOjvEh95/9IVQ9WLwTwg=";
+    rev = "v0.0.1";
+    sha256 = "sha256-Em/jnIUmY1P0WhHX+1W4ALAsIyjrZyMYazMaY7STf3Q=";
   };
 
   dogemap = pkgs.writeScriptBin "run.sh" ''
     #!${pkgs.bash}/bin/bash
-    ${jampuppy}/bin/jampuppy -A index.html --dir ${ui} --host 0.0.0.0 --port 8080
+    ${jampuppy}/bin/jampuppy -A index.html --dir ${ui} --host 0.0.0.0 --port 8080 --proxy '/dogenet http://''${DBX_IFACE_DOGENET_WEB_API_NODES_HOST}:''${DBX_IFACE_DOGENET_WEB_API_NODES_PORT}/dbx/dogenet-web-api-nodes'
   '';
 in
 {
